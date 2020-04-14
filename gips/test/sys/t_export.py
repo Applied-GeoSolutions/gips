26db9f90aca342754f80c526c01f55a2c898bb2c
Nate Rubin <nrubin@appliedgeosolutions.com>
2020-04-14 17:32:57 -0400

    Add vrt export test

--- gips/test/sys/t_export.py
+++ gips/test/sys/t_export.py
@@ -6,8 +6,9 @@ from . import driver_setup
 
 pytestmark = util.sys # skip everything unless --sys
 
-# 'driver': {'product': [ (path, type, data...),...]...}
+# 'driver': {'prodnuct': [ (path, type, data...),...]...}
 from .expected.std_export import expectations, mark_spec
+from .expected.std_export_vrt import expectations as vrt_expectations
 
 @pytest.mark.parametrize("driver, product",
                          util.params_from_expectations(expectations, mark_spec))
@@ -22,3 +23,17 @@ def t_export(export_wrapper, driver, product):
     if not record_mode: # don't evaluate assertions when in record-mode
         assert (outcome.exit_code == 0
                 and expectations[driver][product] == actual)
+
+
+@pytest.mark.parametrize("driver, product",
+                         util.params_from_expectations(expectations, mark_spec))
+def t_export_vrt(export_wrapper, driver, product):
+    record_mode, runner, working_dir = export_wrapper
+    driver_setup.setup_repo_data(driver)
+    args = ('gips_export',) + driver_setup.STD_ARGS[driver] + (
+        '--res', '100', '100', '--outdir', working_dir, '--notld',
+        '-p', product, '--vrt')
+    outcome, actual = runner(*args)
+    if not record_mode:
+        assert (outcome.exit_code == 0
+                and vrt_expectations[driver][product] == actual)
