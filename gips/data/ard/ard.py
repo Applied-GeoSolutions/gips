c6f1d8137c7f0306eaa1036c7feb872fa7a25752
Rick Emery <remery@ags.io>
2020-09-18 13:40:58 -0400

    ST fetch, inventory, and process working

--- gips/data/ard/ard.py
+++ gips/data/ard/ard.py
@@ -19,40 +19,28 @@
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program. If not, see <http://www.gnu.org/licenses/>
-#
-#   PRISM Climate data is copyrighted by the PRISM Climate Group,
-#   Oregon State University.  For information acceptable use, see
-#   http://prism.oregonstate.edu/documents/PRISM_terms_of_use.pdf.
 ################################################################################
 
 import os
-from datetime import datetime, date, timedelta
-from csv import DictReader
+from datetime import datetime, date
 import re
-import ftplib
-import subprocess
-import tempfile
 import requests
-from gips.core import SpatialExtent, TemporalExtent
 from xml.etree import ElementTree
 from backports.functools_lru_cache import lru_cache
+import tarfile
 
-from gips.data.core import Repository, Data
+from gips.data.core import Repository, Data, CloudCoverData
 import gips.data.core
 
-from gips.utils import settings, List2File
+from gips.utils import settings
 from gips import utils
-
-from gippy import GeoImage
-
-from numpy import mean
 import homura
 
 __author__ = "Rick Emery <remery@ags.io>"
 __version__ = '0.1.0'
 
 def path_row(tile_id):
-    """Converts the given landsat tile string into (path, row)."""
+    """Converts the given ARD tile string into (path, row)."""
     return (tile_id[:3], tile_id[3:])
 
 class ardRepository(Repository):
@@ -61,6 +49,11 @@ class ardRepository(Repository):
     _datedir = '%Y%m%d'
     _tile_attribute = 'PR'
 
+    @classmethod
+    def feature2tile(cls, feature):
+        """ Get tile designation from a geospatial feature (i.e. a row) """
+        fldindex = feature.GetFieldIndex(cls._tile_attribute)
+        return str(feature.GetField(fldindex)).zfill(6)
 
 class ardAsset(gips.data.core.CloudCoverAsset):
     Repository = ardRepository
@@ -88,14 +81,15 @@ class ardAsset(gips.data.core.CloudCoverAsset):
 
     _c1_base_pattern = (
         r'^L(?P<sensor>\w)(?P<satellite>\d{2})_CU_(?P<pathrow>\d{6})'
-        r'_(?P<acq_date>\d{8})_(?P<processing_date>\d{8})_C01_V01_'
+        r'_(?P<acq_date>\d{8})_(?P<processing_date>\d{8})'
+        r'_(?P<coll_num>C\d{2})_(?P<version>V\d{2})_'
     )
 
     _latency = 0
     _assets = {
         'ST': {
             'sensors': ['LT5', 'LE7', 'LC8'],
-            'pattern': _c1_base_pattern + r'ST_\.tar$',
+            'pattern': _c1_base_pattern + r'ST\.tar$',
             'latency': _latency,
         },
     }
@@ -113,23 +107,41 @@ class ardAsset(gips.data.core.CloudCoverAsset):
         asset_info['startdate'] = min(startdates)
 
     def __init__(self, filename):
-        """ Inspect a PRISM file """
+        """ Inspect a single file and get some metadata """
         super(ardAsset, self).__init__(filename)
-        bname = os.path.basename(filename)
-        # 'PRISM_<var>_<stability>_<scale&version>_<time period>_bil.zip'
-        _, variable, stability, scalever, date, bilzip = bname.split('_')
-        assert bilzip == 'bil.zip', "didn't tokenize properly."
-        scale = re.sub(r'(.+)D[0-9]+', r'\1', scalever)
-        version = re.sub(r'.+(D[0-9]+)', r'\1', scalever)
-        self.date = datetime.strptime(date, '%Y%m%d').date()
-        self.asset = '_' + variable
-        self.sensor = 'prism'
-        self.scale = scale
-        self.version = version
-        self.stability = stability
-        self._version = self._stab_score[self.stability] * .01 + int(self.version[1:])
-        # only one tile
-        self.tile = 'CONUS'
+
+        fname = os.path.basename(filename)
+
+        utils.verbose_out("Attempting to load " + fname, 2)
+
+        # determine asset type
+        match = None
+        for at, ad in self._assets.items():
+            match = re.match(ad['pattern'], fname)
+            if match:
+                break
+        if match is None:
+            raise RuntimeError(
+                "No valid landsat asset type for '{}'".format(fname), filename)
+        self.asset = at
+
+       
+        self.sensor = "L{}{}".format(match.group('sensor'),
+                                     int(match.group('satellite')))
+        self.date = datetime.strptime(match.group('acq_date'), "%Y%m%d")
+        processing_date = datetime.strptime(match.group('processing_date'),
+                                            '%Y%m%d')
+
+        self.tile = match.group('pathrow')
+        self.collection_number = match.group('coll_num')
+        self.version = match.group('version')
+        smeta = self._sensors[self.sensor]
+        self.meta = {}
+        self.meta['bands'] = {}
+
+        if self.sensor not in self._sensors.keys():
+            raise Exception("Sensor %s not supported: %s" % (self.sensor, filename))
+        self._version = self.version
 
     @classmethod
     def ee_login(cls):
@@ -197,7 +209,7 @@ class ardAsset(gips.data.core.CloudCoverAsset):
                     'dataset': 'ARD_TILE',
                     'a_type': asset,
                     # ignored but required
-                    'basename': result['displayId'] + '.tar.gz',
+                    'basename': result['displayId'] + '_{}.tar'.format(asset),
                     # ignored
                     #'scene_cloud_cover': float(scene_cloud_cover),
                     #'land_cloud_cover': float(land_cloud_cover),
@@ -224,159 +236,46 @@ class ardAsset(gips.data.core.CloudCoverAsset):
     def version_text(self):
         return '{v}-{s}'.format(v=self.version, s=self.stability)
 
-    def datafiles(self):
-        datafiles = super(ardAsset, self).datafiles()
-        datafiles = [d for d in datafiles if d.lower().endswith('.bil')]
-        if len(datafiles) > 0:
-            indexfile = self.filename + '.index'
-            utils.verbose_out('indexfile: {}'.format(indexfile), 3)
-            List2File(datafiles, indexfile)
-            return datafiles
-
-    @classmethod
-    def choose_asset(cls, a_type, tile, date, remote_fn_list):
-        """Choose the most favorable stability & version (often only one)."""
-        return max(
-               (fn for fn in remote_fn_list if date.strftime('%Y%m%d') in fn),
-               key=(lambda x: ardAsset(x)._version))
-
-
-class ardData(Data):
-    """ A tile (CONUS State) of PRISM """
+class ardData(CloudCoverData):
     name = 'ARD'
     version = __version__
     Asset = ardAsset
     inline_archive = True
 
     _lt5_startdate = date(1984, 3, 1)
-    # Prism official docs at http://www.prism.oregonstate.edu/FAQ/ say:
-    # "Dataset values are stored . . . precipitation as millimeters and
-    # temperature as degrees Celsius."
     _products = {
-        'ST': {
+        'st': {
             'description': 'Surface Temperature',
             'assets': ['ST'],
-            'bands': [{'name': 'ppt', 'units': 'mm'}],
             'startdate': Asset._lt5_startdate,
             'latency': Asset._latency,
         },
     }
 
-    @classmethod
-    def normalize_tile_string(cls, tile_string):
-        """'conus' is invalid, but 'CONUS' is valid, so help the user out."""
-        return tile_string.upper()
-
-    def process(self, *args, **kwargs):
-        """Deduce which products need producing, then produce them."""
-        products = super(ardData, self).process(*args, **kwargs)
+    @Data.proc_temp_dir_manager
+    def process(self, products=None, *args, **kwargs):
+        """ Make sure all products have been processed """
+        products = super(ardData, self).process(products, *args, **kwargs)
         if len(products) == 0:
+            utils.verbose_out("Skipping processing; no products requested.", 5)
             return
-        # overwrite = kwargs.get('overwrite', False)
-        # utils.verbose_out('\n\noverwrite = {}\n'.format(overwrite), 2)
-        # TODO: overwrite doesn't play well with pptsum -- wonder if it would
-        #       if it was made into a composite product (which it is)
-        assert len(ardAsset._sensors) == 1  # sanity check to force this code to stay current
-        sensor = list(ardAsset._sensors.keys())[0]
+        if len(self.assets) == 0:
+            utils.verbose_out("Skipping processing; no assets found.", 5)
+            return
 
-        def get_bil_vsifile(d, a):
-            with utils.error_handler('Error accessing asset {}'
-                                     .format(d), continuable=True):
-                return os.path.join(
-                    '/vsizip/' + d.assets[a].filename,
-                    d.assets[a].datafiles()[0])
+        for pr in products.products:
+            # ARD is a product/asset pair, so always use the first (only) asset
+            asset = self.assets[self._products[pr]['assets'][0].upper()]
+            product_name = os.path.basename(
+                asset.filename
+            ).split('.')[0] + ".tif"
 
-        for key, val in products.requested.items():
-            start = datetime.now()
-            # check that we have required assets
-            requiredassets = self.products2assets([val[0]])
-            # val[0] s.b. key w/o product args
-            description = self._products['pptsum']['description']
-            missingassets = []
-            availassets = []
-            vsinames = {}
-
-            for asset in requiredassets:
-                bil = get_bil_vsifile(self, asset)
-                if bil is None:
-                    missingassets.append(asset)
-                else:
-                    availassets.append(asset)
-                    vsinames[asset] = os.path.join(
-                        '/vsizip/' + self.assets[asset].filename,
-                        bil
-                    )
-
-            if not availassets:
-                utils.verbose_out(
-                    'There are no available assets ({}) on {} for tile {}'
-                    .format(str(missingassets), str(self.date), str(self.id)),
-                    5,
-                )
-                continue
-            prod_fn = '{}_{}_{}.tif'.format(self.basename, 'prism', key)
-            archived_fp = os.path.join(self.path, prod_fn) # final destination
-            if val[0] in ['ppt', 'tmin', 'tmax', 'vrtppt']:
-                with self.make_temp_proc_dir() as tmp_dir:
-                    tmp_fp = os.path.join(tmp_dir, prod_fn)
-                    if val[0] == 'vrtppt':
-                        subprocess.check_call([
-                            'gdalbuildvrt',
-                            tmp_fp,
-                            vsinames[self._products[key]['assets'][0]],
-                        ])
-                    else:
-                        os.symlink(vsinames[self._products[key]['assets'][0]], tmp_fp)
-                    os.rename(tmp_fp, archived_fp)
-            elif val[0] == 'pptsum':
-                if len(val) < 2:
-                    lag = 3 # no argument provided, use default lag of 3 days SB configurable.
-                    prod_fn = re.sub(r'\.tif$', '-{}.tif'.format(lag), prod_fn)
-                    archived_fp = os.path.join(self.path, prod_fn) # have to regenerate, sigh
-                    utils.verbose_out('Using default lag of {} days.'.format(lag), 2)
-                else:
-                    with utils.error_handler("Error for pptsum lag value '{}').".format(val[1])):
-                        lag = int(val[1])
-
-                date_spec = '{},{}'.format(
-                    datetime.strftime(
-                        self.date - timedelta(days=lag), '%Y-%m-%d',
-                    ),
-                    datetime.strftime(self.date, '%Y-%m-%d'),
-                )
-                inv = self.inventory(dates=date_spec, products=['ppt'],)
-                inv.process()
-                # because DataInventory object doesn't update
-                inv = self.inventory(dates=date_spec, products=['ppt'],)
-                if len(inv.data) < lag:
-                    utils.verbose_out(
-                        '{}: requires {} preceding days ppt ({} found).'
-                        .format(key, lag, len(inv.data)),
-                        3,
-                    )
-                    continue  # go to next product to process
-                imgs = []
-                asset_fns = [] # have to grab filenames for multiple days
-                for tileobj in inv.data.values():
-                    data_obj = next(iter(tileobj.tiles.values()))
-                    asset_fns.append(os.path.basename(data_obj.assets['_ppt'].filename))
-                    imgs.append(GeoImage(get_bil_vsifile(data_obj, '_ppt')))
-
-                with self.make_temp_proc_dir() as tmp_dir:
-                    tmp_fp = os.path.join(tmp_dir, prod_fn)
-                    oimg = GeoImage.create_from(imgs[0], tmp_fp)
-                    oimg.set_nodata(-9999)
-                    oimg.set_bandname(
-                        description + '({} day window)'.format(lag), 1
-                    )
-                    oimg.add_meta(self.prep_meta(sorted(asset_fns)))
-                    for chunk in oimg.chunks():
-                        oarr = oimg[0].read(chunk) * 0.0 # wat
-                        for img in imgs:
-                            oarr += img[0].read(chunk)
-                        oimg[0].write(oarr, chunk)
-                    oimg.save()
-                    os.rename(tmp_fp, archived_fp)
-                oimg = None  # help swig+gdal with GC
-            self.AddFile(sensor, key, archived_fp)  # add product to inventory
-        return products
+            fname = self.temp_product_filename(asset.sensor, 'st')
+            temp_dir = os.path.dirname(fname)
+            out_name = os.path.basename(fname)
+            tarfile.open(asset.filename).extractall(path=temp_dir)
+            os.rename(
+                os.path.join(temp_dir, product_name),
+                os.path.join(temp_dir, out_name)
+            )
+            self.archive_temp_path(fname)
